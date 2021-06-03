34069e89638b29d2a57910aa51e277074f7f6e52
jr1412594 <jr.medina1412@gmail.com>
2021-06-03 15:24:37 -0600

    add the delete method

---
+++ the_delete_method.rb
@@ -0,0 +1,14 @@
+# puts "hello world".delete("word")
+checker = %i[what will this be]
+
+# sentence = "pierogi"
+
+# def custom_delete(string, substring)
+#     final = ""
+#     string.chars.each { |elem| final << elem unless substring.include?(elem) }
+#     final
+# end
+
+# p custom_delete(sentence, "p") #ierogi
+# p custom_delete(sentence, "ig") #pero
+# p custom_delete(sentence,"o") == sentence.delete("o")
