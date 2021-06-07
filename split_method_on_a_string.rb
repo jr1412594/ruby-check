50b618a33f6791ee9913b702e24b8024b4260621
jr1412594 <jr.medina1412@gmail.com>
2021-06-06 22:53:09 -0600

    more string iteration

--- split_method_on_a_string.rb
+++ split_method_on_a_string.rb
@@ -1,6 +1,14 @@
-sentence = "hi, my name is jr. There are spaces here!"
+# sentence = "hi, my name is jr. There are spaces here!"
 
-words = sentence.split(" ")
-words.each { |word| puts word.length}
+# words = sentence.split(" ")
+# words.each { |word| puts word.length}
 
 #Split will turn a string into an array for you to iterate over.
+
+sentence = 'Dont forget how to do certain things because this will throw you off.'
+
+
+    words = sentence.split(' ')
+    words.each do |word|
+        puts word.length
+    end
