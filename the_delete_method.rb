c72077496dc49ea3cfb18beb5e594ca5d7a07729
jr1412594 <jr.medina1412@gmail.com>
2021-06-06 22:49:33 -0600

    delete mod

--- the_delete_method.rb
+++ the_delete_method.rb
@@ -1,14 +1,14 @@
 # puts "hello world".delete("word")
 checker = %i[what will this be]
 
-# sentence = "pierogi"
+sentence = "pierogi"
 
-# def custom_delete(string, substring)
-#     final = ""
-#     string.chars.each { |elem| final << elem unless substring.include?(elem) }
-#     final
-# end
+def custom_delete(string, substring)
+    final = ""
+    string.chars.each { |elem| final << elem unless substring.include?(elem) }
+    final
+end
 
-# p custom_delete(sentence, "p") #ierogi
-# p custom_delete(sentence, "ig") #pero
-# p custom_delete(sentence,"o") == sentence.delete("o")
+p custom_delete(sentence, "p") #ierogi
+p custom_delete(sentence, "ig") #pero
+p custom_delete(sentence,"o") == sentence.delete("o")
