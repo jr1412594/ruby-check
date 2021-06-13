9ddf4c665de5722f44933ddd60900f65e86da003
jr1412594 <jr.medina1412@gmail.com>
2021-06-13 14:55:30 -0600

    sample

---
+++ sample_method.rb
@@ -0,0 +1,9 @@
+flavors = ['chocolate', 'vanilla', 'strawberry', 'rum raisin']
+
+puts flavors.sample(2)
+
+=begin
+The sample method will randomly pick an element out of an array
+can accept an argument and if argument is specified
+the return will be in an array
+=end
