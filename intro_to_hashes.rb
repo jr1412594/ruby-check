e88766b44e53a9b0c8bdbbc943434f0e7f864229
jr1412594 <jr.medina1412@gmail.com>
2021-06-13 23:04:32 -0600

    hashes

---
+++ intro_to_hashes.rb
@@ -0,0 +1,27 @@
+# empty_hash = {}
+
+# p empty_hash
+# p empty_hash.class
+
+
+nfl_roster = {'Tom Brady' => 'New England Patriots',
+              'Tony Romo' => 'Dallas Cowboys',
+              'Rob Gronkowski' => 'New England Patriots',
+              'Arron Rogers' => 'Denver Broncos',
+              'Tim Tebow' => 'Jacksonvill Jaguars'}
+
+nba_roster = {
+  'Cleveland Caviliers' => [
+    "Lebron",
+    'Love',
+    'Irving'
+  ],
+  'Goldenstate Warriors' => [
+    'Curry',
+    'Thompson',
+    'Durrant'
+  ]
+}
+
+# p nfl_roster['Arron Rogers']
+p nba_roster['Cleveland Caviliers'][1]
