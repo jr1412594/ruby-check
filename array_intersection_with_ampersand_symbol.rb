8ae3857e41bc70cbe4d6df3f362296a278511b94
jr1412594 <jr.medina1412@gmail.com>
2021-06-13 22:14:58 -0600

    intersection with amperand

---
+++ array_intersection_with_ampersand_symbol.rb
@@ -0,0 +1,11 @@
+a1 = [1, 1, 2, 3, 4, 5] & 
+
+a2 = [1, 4, 5, 8, 9]
+
+def custom_intersection(arr1, arr2)
+    final = []
+    arr1.each { |element| final << element if arr2.include?(element) } 
+    final
+end
+
+p custom_intersection(a1, a2) == a1 & a2
