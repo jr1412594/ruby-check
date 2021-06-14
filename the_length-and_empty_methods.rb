dfa4d26e5606d95840ff58e62069ade6a32c8f02
jr1412594 <jr.medina1412@gmail.com>
2021-06-14 13:09:12 -0600

    length and empty? methods on a hash

---
+++ the_length-and_empty_methods.rb
@@ -0,0 +1,16 @@
+=begin
+.length will count the amount of key value pairs in the hash
+.empty? will retrurn a boolean (true of false) 
+=end
+
+shopping_list = {
+  bananas: 5,
+  oranges: 10,
+  carrots: 3,
+  crackers: 15
+}
+
+vegan_shopping_list = {}
+
+p shopping_list.empty?
+p vegan_shopping_list.empty?
