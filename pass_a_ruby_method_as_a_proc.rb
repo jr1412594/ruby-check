8ad4dcf3fb2fac259908619f7d78e8f8146bbc98
jr1412594 <jr.medina1412@gmail.com>
2021-06-20 10:26:41 -0600

    ruby method to proc

---
+++ pass_a_ruby_method_as_a_proc.rb
@@ -0,0 +1,11 @@
+# p ["1", "2", "3"].map(&:to_i)
+
+# p [10, 20, 30].map(&:to_s)
+
+
+
+# sentence_array = %w(what the what pierogi we are going to turn this into uppercase)
+
+# p sentence_array.map(&:upcase)
+
+p [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].select(&:even?)
