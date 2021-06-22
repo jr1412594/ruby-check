bf5b8f90b539658caf2d0efa8ec2ac59364cafb4
jr1412594 <jr.medina1412@gmail.com>
2021-06-22 17:03:25 -0600

    scan method

---
+++ the_scan_method.rb
@@ -0,0 +1,5 @@
+voicemail = "I can be reached at 555-123-4567 or regexman@gmail.com"
+
+p voicemail.scan(/[aeiou]/)
+
+# Regex /[aeiou]/ will looka for any character in the brackets regardless of order
