8d5669cf22c3319385780d167c7fc6b34afe20b3
jr1412594 <jr.medina1412@gmail.com>
2021-06-28 22:48:44 -0600

    getter methods for custom classes

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -9,15 +9,26 @@ class Gadget
     end
 
     def to_s
-        "Gadget #{@production_number} has the username #{@username}"
+        "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}"
+        # binding.pry
+    end
+
+    def username
+        @username
+    end
+
+    def production_number
+        @production_number
     end
 
 end
 
 phone = Gadget.new
 
-puts phone.respond_to?(:to_s)
-puts phone.to_s
+p phone.username
+p phone.production_number
 
-binding.pry
+
+# binding.pry
+
 
