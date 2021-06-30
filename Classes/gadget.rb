d4c89f3b624ecaa8ae616d262bea02c1fc01f9e0
jr1412594 <jr.medina1412@gmail.com>
2021-06-29 22:33:07 -0600

    private methods

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -6,56 +6,32 @@ class Gadget
     attr_reader :production_number #, :username  // if you wanted multiple args
     attr_writer :password
 
-    @@all = []
-
     def initialize(username, password)
         @username = username
         @password = password
-        @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
-        @@all << self
+        @production_number = generate_production_number
     end
 
     def to_s
-        "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id} and #{@password}"
+        "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the ID #{self.object_id}."
         # binding.pry
     end
 
-    def self.all
-        @@all
-    end
+    private
 
-    def self.all_usernames
-        usernames_array = []
-        self.all.each do |gadget|  
-            usernames_array << gadget.username
-        end
-        return usernames_array
-    end
-
-    def self.all_production_numbers
-        production_numbers_array = []
-        self.all.each do |gadget|
-            production_numbers_array << gadget.production_number
-        end
-        production_numbers_array
+    def generate_production_number
+        start_digits = rand(10000..99999)
+        end_digits = rand(10000..99999)
+        alphabet = ("A".."Z").to_a
+        middle_digits = '2017'
+        5.times { middle_digits << alphabet.sample }
+        "#{start_digits}-#{middle_digits}-#{end_digits}"
     end
 
 end
 
-g1 = Gadget.new("rubyfan102", "Programmer123")
-g2 = Gadget.new("noneya", "bestPass")
-g3 = Gadget.new("Gadgetnumber3", "sumosauce82")
-
-# p g1.username
-# p g1.production_number
-# g1.password = "haha"
-# g1.username = 'what_the_what'
-# p g1.username
-
-# p Gadget.all
-# p Gadget.all_usernames
-p Gadget.all_production_numbers
-
+phone = Gadget.new("user", "password")
+p phone.generate_production_number
 
 # binding.pry
 
