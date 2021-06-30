a4c801655483a403cb3ba59a9292caab1c0a4bbf
jr1412594 <jr.medina1412@gmail.com>
2021-06-29 23:26:59 -0600

    multiple setter methods

--- Classes/gadget.rb
+++ Classes/gadget.rb
@@ -3,13 +3,14 @@ require 'pry'
 class Gadget
 
     # attr_writer :password
-    attr_reader :production_number #, :username  // if you wanted multiple args
+    attr_reader :production_number, :apps#, :username  // if you wanted multiple args
     attr_accessor :username
 
     def initialize(username, password)
         @username = username
         @password = password
         @production_number = generate_production_number
+        @apps = []
     end
 
     def to_s
@@ -17,11 +18,19 @@ class Gadget
         # binding.pry
     end
 
+    def reset(username, password)
+        self.username = username
+        self.password = new_password
+        self.apps = []
+    end
+
     def password=(new_password)
         @password = new_password if validate_password(new_password)
     end
 
     private
+
+    attr_writer :apps
 
     def generate_production_number
         start_digits = rand(10000..99999)
