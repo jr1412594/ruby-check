e214efb3e4b83eb2b740ce774957dd5fbe883904
jr1412594 <jr.medina1412@gmail.com>
2021-06-29 22:48:45 -0600

    protected methods within classes

---
+++ Classes/protected_methods.rb
@@ -0,0 +1,24 @@
+=begin
+Protected Methods
+
+Review of Methods
+    public methods allow interaction with an object
+
+    public methods can be called by any other object
+
+    private methods can onlhy be called within the object
+
+    private methods cannot be invoked by another object.
+
+    private methods cannot be called with an explicit receiver --
+    only the current object is able to receive the method
+
+What is a protected method?
+
+    protected methods can be invoked only by objects of the same class
+
+    protected methods can be called within the same family of objects
+
+    protected methods are used to compare objects of the same class
+=end
+
