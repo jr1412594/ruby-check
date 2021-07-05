3e1b0ee747a996ab760d8651b3c8ad944fde94aa
jr1412594 <jr.medina1412@gmail.com>
2021-07-04 22:28:34 -0600

    shown partition

--- the_partition_method.rb
+++ the_partition_method.rb
@@ -1,3 +1,48 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1, -2, -3, -4, -5, -6, -7]
+
+#Expected output [10, -28]
+
+def partition_method(list)
+    #names of both arrays....
+                        #partition method is called on list
+                                        #block if number is positive it goes in positive array
+                                        #else it goes to negative array
+    positive, negative = list.partition { |number| number.positive?}
+    #call length of positive array and sum of negative array
+    [positive.length, negative.sum]
+end
+
+p partition_method(numbers)
+
+
+def positive_count_negative_sum(list)
+    #Initialize my positive and negative arrays to push in values
+    positive = []
+    negative = []
+    #use each method to go over each number in the list
+    list.each do |number|
+        #ternary that checks the number and decides what array it belongs in
+        number >= 0 ? positive << number : negative << number
+    end
+    #returning the length of the positive array and sum of the negative array
+    [positive.length, negative.sum]
+end
+
+puts "The answer is..."
+p positive_count_negative_sum(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # foods = ["Steak","Vegetaables", "Steak Burger", "Kale", "Tofu", "Tuna Steaks"]
 
 # good = foods.select { |food| food.include?("Steak") }
@@ -10,17 +55,17 @@
 
 # p good
 # p bad
-numbers = [3, 14, 13, 9, 35, 23, 46, 16, 27, 37, 73]
+# numbers = [3, 14, 13, 9, 35, 23, 46, 16, 27, 37, 73]
 
-even, odd = numbers.partition { |number| number.even? }
+# even, odd = numbers.partition { |number| number.even? }
 
-p even
-p odd
+# p even
+# p odd
 
 
-second_numbers = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# second_numbers = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-positive, negative = second_numbers.partition { |number| number.positive? }
+# positive, negative = second_numbers.partition { |number| number.positive? }
 
-p positive
-p negative
+# p positive
+# p negative
