62861f6615fa96332694facd69e9efa790a754af
jr1412594 <jr.medina1412@gmail.com>
2021-07-05 20:48:12 -0600

    hash continued

--- Classes/candidate.rb
+++ Classes/candidate.rb
@@ -2,22 +2,30 @@ class Candidate
 
     attr_accessor :name, :age, :occupation, :hobby, :birthplace
 
-    def initialize(name, details)
+    def initialize(name, details = {})
+        defaults = {
+            age: 35,
+            occupation: "Candidate",
+            hobby: "Running for office",
+            birthplace: "The U.S. of A"
+        }
+        defaults.merge!(details)
+
         @name = name
-        @age = details[:age]
-        @occupation = details[:occupation]
-        @hobby = details[:hobby]
-        @birthplace = details[:birthplace]
+        @age = defaults[:age]
+        @occupation = defaults[:occupation]
+        @hobby = defaults[:hobby]
+        @birthplace = defaults[:birthplace]
     end
 end
 
-info = {
-    hobby: "Fishing",
-    occupation: "Banker",
-    age: 53,
-    birthplace: "Kentucky"
-}
-senator = Candidate.new("Mr.Smith", info)
+# info = {
+#     hobby: "Fishing",
+#     occupation: "Banker",
+#     age: 53,
+#     birthplace: "Kentucky"
+# }
+senator = Candidate.new("Mr.Smith", hobby: "horror movies", occupation: "popcorn vender")
 
 p senator.name
 p senator.age
